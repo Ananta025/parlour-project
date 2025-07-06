b5b4ba865aab7cb5c0f71dac8097fdce7f8f7d7c
Ananta Chandra Das <danantach@gmail.com>
2025-07-06 18:58:45 +0530

    Working on deploy

--- backend-parlour-api/types/socket-override.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'socket.io' {
-  import { Server as HttpServer } from 'http';
-  import { Server as IOServer, Socket } from 'socket.io';
-
-  export { IOServer as Server, Socket };
-}
